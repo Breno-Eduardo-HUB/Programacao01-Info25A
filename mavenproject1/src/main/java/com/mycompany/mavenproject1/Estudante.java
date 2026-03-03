47f1bcd72e77456f0741f2e8f3dc2641e2103161
aluno <aluno@lab01-29>
2026-03-03 10:58:17 -0300

    classe

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Estudante.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Estudante.java
@@ -4,10 +4,22 @@
  */
 package com.mycompany.mavenproject1;
 
+import java.util.Date;
+
 /**
  *
  * @author aluno
  */
 public class Estudante {
+    private String nome;
+    private Date dataNascimento;
+    private char genero;
+    private String matricula;
+    private String email;
     
+    public int obterIdade (Date hoje){
+        int idade = 0;
+        //Lógica para calcular idade
+        return idade;
+    }
 }
