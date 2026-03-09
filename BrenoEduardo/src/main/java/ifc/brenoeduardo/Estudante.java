0152718127a5c28821061d4bd4dfd322aa42a004
aluno <aluno@lab02-19>
2026-03-09 14:12:42 -0300

    sla67

---
+++ BrenoEduardo/src/main/java/ifc/brenoeduardo/Estudante.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ifc.brenoeduardo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aluno
+ */
+public class Estudante {
+    private String nome;
+    private Date dataNascimento;
+    private char genero;
+    private String matricula;
+    private String email;
+    
+        public Estudante(){
+                this.nome= "nobody";
+        
+        }
+    
+        public int obterIdade (Date hoje){
+        int idade = 0;
+        //Lógica para calcular idade
+        return idade;
+    }
+        public String getNome(){
+            return this.nome;
+        }
+        public void setNome (String _nome){
+            this.nome = _nome;
+        }
+}
