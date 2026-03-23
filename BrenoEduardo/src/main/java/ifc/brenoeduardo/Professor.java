802da2426d9087fa3ce6968a7df4772cb786f680
suporte <suporte@191.52.13.170>
2026-03-23 13:29:55 -0300

    começo de aula

---
+++ BrenoEduardo/src/main/java/ifc/brenoeduardo/Professor.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ifc.brenoeduardo;
+
+import java.util.Date;
+
+public class Professor {
+    
+    private static String nome;
+    private static String STAPE;
+    private String email;
+    private String telefone;
+    private String TitulacaoAcademica;
+    private String areaDeAtuacao;
+    private String Formacao;
+    private Date DataDeNascimento;
+    private String cpf;
+    private String genero;
+}
