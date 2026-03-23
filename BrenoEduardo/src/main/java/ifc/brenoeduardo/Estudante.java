8d7dd4feac1afa5d5ec8c3c7da95858fa9970805
suporte <suporte@191.52.13.170>
2026-03-23 13:20:33 -0300

    começo de aula

--- BrenoEduardo/src/main/java/ifc/brenoeduardo/Estudante.java
+++ BrenoEduardo/src/main/java/ifc/brenoeduardo/Estudante.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package ifc.brenoeduardo;
 
 import java.util.Date;
@@ -11,27 +10,40 @@ import java.util.Date;
  * @author aluno
  */
 public class Estudante {
+
     private static String nome;
     private Date dataNascimento;
-    char genero;
+    private char genero;
     private String matricula;
     private String email;
-    
-        public Estudante(){
-                this.nome= "nobody";
-            
-        }
-    
-        public int obterIdade (Date hoje){
+    private String nivelEnsino;
+    private String telefone;
+    private String dataDeIngresso;
+    private String SemestreInicial;
+    private String SituacaoAcademica;
+    private String digito;
+
+    public Estudante() {
+        this.nome = "nobody";
+
+    }
+
+    public int obterIdade(Date hoje) {
         int idade = 0;
         //Lógica para calcular idade
         return idade;
     }
-        public static String getNome(){
-            return nome;
-        }
-        public static void setNome (String _nome){
-            nome = _nome;
-        }
 
+    public static String getNome() {
+        return nome;
+    }
+
+    public static void setNome(String _nome) {
+        nome = _nome;
+    }
+
+    public static void setMatricula(String digito) {
+        digito = digito;
+
+    }
 }
