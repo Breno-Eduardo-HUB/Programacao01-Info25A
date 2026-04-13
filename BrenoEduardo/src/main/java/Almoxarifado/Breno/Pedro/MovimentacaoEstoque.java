c00d97cb0de514a97d3e3b4f09cb36f5fb3dca7a
suporte <suporte@lab02-17>
2026-04-13 14:18:32 -0300

    atividade

---
+++ BrenoEduardo/src/main/java/Almoxarifado/Breno/Pedro/MovimentacaoEstoque.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Almoxarifado.Breno.Pedro;
+
+import java.util.Date;
+
+public class MovimentacaoEstoque {
+    private String identificadorModificador;
+    private String tipo;
+    private int quantidade;
+    private Date dataHora;
+    private String item;
+    private String usuario;
+    
+}
---
+++ BrenoEduardo/src/main/java/Almoxarifado/Breno/Pedro/item.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Almoxarifado.Breno.Pedro;
+
+/**
+ *
+ * @author suporte
+ */
+public class item {
+    
+    private String IdentificadorItem;
+    private String nome;
+    private String descrição;
+    private String categoria;
+    private String unidadeMedida;
+    private int quantidadeAtual;
+    private int quantidadeMinima;
+    private String localizacao;
+    private boolean status;
+    
+    
+    
+    
+}
